04bce37e754e155e53be890f239e873c2ac532f3
Ioanna Xenaki <ixenaki@elder.org>
2021-04-12 22:17:48 +0100

    Build mode: production

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
-const webpackMerge = require('webpack-merge')
+const webpackMerge = require("webpack-merge")
+
+const modeConfig = (env) => require(`./build-utils/webpack.${env}`)
 
 module.exports = ({ mode = "production" }) =>
-  webpackMerge({
-    entry: "./src",
-    mode,
-    output: {
-      filename: "bundle.[contenthash].js",
-    },
-    plugins: [
-      new HtmlWebpackPlugin({ title: "Intro to Webpack" }),
-      new CleanWebpackPlugin(),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.(png|jpe?g)$/i,
-          use: [
-            {
-              loader: "file-loader",
-            },
-          ],
-        },
+  webpackMerge(
+    {
+      entry: "./src",
+      mode,
+      output: {
+        filename: "bundle.js",
+      },
+      plugins: [
+        new HtmlWebpackPlugin({ title: "Intro to Webpack" }),
+        new CleanWebpackPlugin(),
       ],
+      module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.(png|jpe?g)$/i,
+            use: [
+              {
+                loader: "file-loader",
+              },
+            ],
+          },
+        ],
+      },
     },
-  })
+    modeConfig(mode)
+  )
