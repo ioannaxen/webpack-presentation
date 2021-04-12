5156637079356373d15524747bff485b9dd3eb2e
Ioanna Xenaki <ixenaki@elder.org>
2021-04-12 17:21:23 +0100

    Tree shaking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import makeTitle from "./components/title"
 import makeImage from "./components/image"
-
-const title = makeTitle()
-document.body.appendChild(title)
 
 const image = makeImage()
 document.body.appendChild(image)
