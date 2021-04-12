0a03079204e0a6c8ae4ea47ee61e778b158b3771
Ioanna Xenaki <ixenaki@elder.org>
2021-04-12 22:04:54 +0100

    Adding webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const webpackMerge = require('webpack-merge')
 
-module.exports = {
-  entry: "./src",
-  mode: "development",
-  output: {
-    filename: "bundle.[contenthash].js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ title: "Intro to Webpack" }),
-    new CleanWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpe?g)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
+module.exports = ({ mode = "production" }) =>
+  webpackMerge({
+    entry: "./src",
+    mode,
+    output: {
+      filename: "bundle.[contenthash].js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ title: "Intro to Webpack" }),
+      new CleanWebpackPlugin(),
     ],
-  },
-}
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+      ],
+    },
+  })
