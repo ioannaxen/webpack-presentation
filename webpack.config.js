77a956179c1b028619a666c76e948906365e1fab
Ioanna Xenaki <ixenaki@elder.org>
2021-04-12 18:00:45 +0100

    Using HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
 module.exports = {
   entry: "./src",
   mode: "development",
   output: {
     filename: "bundle.[contenthash].js",
   },
+  plugins: [new HtmlWebpackPlugin({ title: "Intro to Webpack" })],
 }
